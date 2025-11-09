bbdace5714e25d6cecef2c8b22ba98f7d7a287c5
Abhishek <99063047+kabhi2004@users.noreply.github.com>
2025-11-09 19:49:24 +0530

    Time: 0 ms (100%), Space: 43.4 MB (5.58%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int k = 0; // Pointer for the position of the next valid element
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k; // Number of elements not equal to val
+    }
+}
