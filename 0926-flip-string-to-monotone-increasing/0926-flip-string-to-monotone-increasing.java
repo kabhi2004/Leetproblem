10e4c9fd950465736fd624546e528d808e536aa7
Abhishek <99063047+kabhi2004@users.noreply.github.com>
2026-07-21 23:31:53 +0530

    Time: 7 ms (97.41%), Space: 46.5 MB (96.26%) - LeetHub

---
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minFlipsMonoIncr(String s) {
+
+        int ones = 0;
+        int flips = 0;
+
+        for (char c : s.toCharArray()) {
+
+            if (c == '1') {
+                ones++;
+            } else {
+                flips = Math.min(flips + 1, ones);
+            }
+        }
+
+        return flips;
+    }
+}
