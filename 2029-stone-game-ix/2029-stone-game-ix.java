1eb57bcd12766763cca8274c4afeb0e2c95d6e96
Abhishek <99063047+kabhi2004@users.noreply.github.com>
2026-08-16 20:37:54 +0530

    Time: 7 ms (29.05%), Space: 114.9 MB (52.03%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean stoneGameIX(int[] stones) {
+        int count0 = 0;
+        int count1 = 0;
+        int count2 = 0;
+
+        for (int stone : stones) {
+            int rem = stone % 3;
+
+            if (rem == 0) {
+                count0++;
+            } else if (rem == 1) {
+                count1++;
+            } else {
+                count2++;
+            }
+        }
+
+        // count0 is even
+        if (count0 % 2 == 0) {
+            return count1 > 0 && count2 > 0;
+        }
+
+        // count0 is odd
+        return Math.abs(count1 - count2) > 2;
+    }
+}
