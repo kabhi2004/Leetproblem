080b5e371421762769d3ffb5b4f68bfb70f01cb6
Abhishek <99063047+kabhi2004@users.noreply.github.com>
2026-09-02 22:27:05 +0530

    Time: 0 ms (100%), Space: 44.7 MB (99.02%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        
+        // For n >= 2, it is always possible to construct
+        // an array where all elements have the same parity.
+        return true;
+    }
+}
